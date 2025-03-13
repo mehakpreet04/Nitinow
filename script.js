fde64f58962055305d1bdbb94e5d6bad96791ba2
Mehak Preet <136305579+mehakpreet04@users.noreply.github.com>
2025-03-13 16:28:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const translations = {
     "Education": { "hi": "शिक्षा", "en": "Education" },
     "Healthcare": { "hi": "स्वास्थ्य देखभाल", "en": "Healthcare" },
     "Agriculture": { "hi": "कृषि", "en": "Agriculture" },
-    "Scholarship Scheme for Students": { "hi": "छात्रों के लिए छात्रवृत्ति योजना", "en": "Scholarship Scheme for Students" },
-    "Mid-Day Meal Scheme": { "hi": "मध्याह्न भोजन योजना", "en": "Mid-Day Meal Scheme" },
-    "Ayushman Bharat Yojana": { "hi": "आयुष्मान भारत योजना", "en": "Ayushman Bharat Yojana" },
-    "Janani Suraksha Yojana": { "hi": "जननी सुरक्षा योजना", "en": "Janani Suraksha Yojana" },
-    "PM Kisan Samman Nidhi": { "hi": "प्रधानमंत्री किसान सम्मान निधि", "en": "PM Kisan Samman Nidhi" },
-    "Soil Health Card Scheme": { "hi": "मृदा स्वास्थ्य कार्ड योजना", "en": "Soil Health Card Scheme" }
+    "Skill India Mission": { "hi": "कौशल भारत मिशन", "en": "Skill India Mission" },
+    "PM Poshan Mid-Day Meal Scheme": { "hi": "प्रधानमंत्री पोषण मध्याह्न भोजन योजना", "en": "PM Poshan Mid-Day Meal Scheme" },
+    "Pradhan Mantri Kaushal Vikas Yojana": { "hi": "प्रधानमंत्री कौशल विकास योजना", "en": "Pradhan Mantri Kaushal Vikas Yojana" },
+    "Beti Bachao Beti Padhao": { "hi": "बेटी बचाओ बेटी पढ़ाओ", "en": "Beti Bachao Beti Padhao" },
+    "Ayushman Bharat – Pradhan Mantri Jan Arogya Yojana": { "hi": "आयुष्मान भारत-प्रधानमंत्री जन आरोग्य योजना", "en": "Ayushman Bharat – Pradhan Mantri Jan Arogya Yojana" },
+    "Pradhan Mantri Suraksha Bima Yojana": { "hi": "प्रधानमंत्री सुरक्षा बीमा योजना", "en": "Pradhan Mantri Suraksha Bima Yojana" },
+    "Rashtriya Bal Swasthya Karyakram": { "hi": "राष्ट्रीय बाल स्वास्थ्य कार्यक्रम", "en": "Rashtriya Bal Swasthya Karyakram" },
+    "Pradhan Mantri Kisan Samman Nidhi": { "hi": "प्रधानमंत्री किसान सम्मान निधि", "en": "Pradhan Mantri Kisan Samman Nidhi" }
+    "Pradhan Mantri Fasal Bima Yojana": { "hi": "प्रधानमंत्री फसल बीमा योजना", "en": "Pradhan Mantri Fasal Bima Yojana" }
+    "Kisan Credit Card (KCC) Scheme": { "hi": "किसान क्रेडिट कार्ड (केसीसी) योजना", "en": "Kisan Credit Card (KCC) Scheme" }
 };
 
 function translatePage(lang) {
